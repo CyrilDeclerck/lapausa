38f9097a7dffe02e3c024e5fcc5e946e5b228304
Cyril Declerck <cdeclerck@dalt.fr>
2024-03-01 21:30:38 +0100

    remove routes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,51 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  nitro: {
-    prerender: {
-      routes: [
-        "/_ipx/_/home.webp",
-        "/_ipx/_/home-d.webp",
-        "/_ipx/f_webp/home-d.webp",
-        "/_ipx/f_webp/home.webp",
-        //intérieur
-        "/_ipx/f_webp&s_750x760/interieur/chambre.jpg",
-        "/_ipx/f_webp&s_750x760/interieur/chambre-1.jpg",
-        "/_ipx/f_webp&s_750x760/interieur/cuisine-salon.jpg",
-        "/_ipx/f_webp&s_750x760/interieur/douche.jpg",
-        "/_ipx/f_webp&s_750x760/interieur/photo-ambiance-bouquin-cuisine.jpg",
-        "/_ipx/f_webp&s_750x760/interieur/photo-ambiance-cuisine-citron.jpg",
-        "/_ipx/f_webp&s_750x760/interieur/photo-ambiance-echelle.jpg",
-        "/_ipx/f_webp&s_750x760/interieur/salon-escalier.jpg",
-        "/_ipx/f_webp&s_750x760/interieur/sdb-vue-mer.jpg",
-        "/_ipx/f_webp&s_750x760/interieur/wc.jpg",
-        "/_ipx/f_webp&s_1500x1520/interieur/chambre.jpg",
-        "/_ipx/f_webp&s_1500x1520/interieur/chambre-1.jpg",
-        "/_ipx/f_webp&s_1500x1520/interieur/cuisine-salon.jpg",
-        "/_ipx/f_webp&s_1500x1520/interieur/douche.jpg",
-        "/_ipx/f_webp&s_1500x1520/interieur/photo-ambiance-bouquin-cuisine.jpg",
-        "/_ipx/f_webp&s_1500x1520/interieur/photo-ambiance-cuisine-citron.jpg",
-        "/_ipx/f_webp&s_1500x1520/interieur/photo-ambiance-echelle.jpg",
-        "/_ipx/f_webp&s_1500x1520/interieur/salon-escalier.jpg",
-        "/_ipx/f_webp&s_1500x1520/interieur/sdb-vue-mer.jpg",
-        "/_ipx/f_webp&s_1500x1520/interieur/wc.jpg",
-        //extérieur
-        "_ipx/f_webp&s_1500x1520/exterieur/coucher-de-soleil.jpg",
-        "_ipx/f_webp&s_1500x1520/exterieur/vue-colline.jpg",
-        "_ipx/f_webp&s_1500x1520/exterieur/vue-mer-panoramique.jpg",
-        "_ipx/f_webp&s_750x760/exterieur/coucher-de-soleil.jpg",
-        "_ipx/f_webp&s_750x760/exterieur/vue-colline.jpg",
-        "_ipx/f_webp&s_750x760/exterieur/vue-mer-panoramique.jpg",
-        //activités
-        "_ipx/f_webp&s_1500x1520/exterieur/camogli.jpg",
-        "_ipx/f_webp&s_1500x1520/exterieur/portofino.jpg",
-        "_ipx/f_webp&s_1500x1520/exterieur/sestri-baia.webp",
-        "_ipx/f_webp&s_750x760/exterieur/camogli.jpg",
-        "_ipx/f_webp&s_750x760/exterieur/portofino.jpg",
-        "_ipx/f_webp&s_750x760/exterieur/sestri-baia.webp",
-      ],
-    },
-  },
   modules: [
     "@nuxtjs/tailwindcss",
     "@nuxtjs/i18n",
